0fe0c0b1940b5fdb13f47bde85d71a100834461e
mithunm-63 <mithun063.m@gmail.com>
2026-09-05 14:28:57 +0530

    feat: support scoped git file history queries

--- backend/src/main/java/com/codeintel/history/GitFileChangeRecordRepository.java
+++ backend/src/main/java/com/codeintel/history/GitFileChangeRecordRepository.java
@@ -2,8 +2,10 @@ package com.codeintel.history;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface GitFileChangeRecordRepository extends JpaRepository<GitFileChangeRecord, String> {
     List<GitFileChangeRecord> findByCommit_Project_Id(String projectId);
+    List<GitFileChangeRecord> findByCommit_IdIn(Collection<String> commitIds);
 }
