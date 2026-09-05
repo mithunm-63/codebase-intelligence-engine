dd0af91013314c77be69520c2395b65a9b7f58cd
mithunm-63 <mithun063.m@gmail.com>
2026-09-05 12:48:42 +0530

    chore: remove migrated Dependency Sentinel files

--- dependency-sentinel/backend/src/main/java/com/dependency/sentinel/project/ProjectRepository.java
+++
@@ -1,5 +0,0 @@
-package com.dependency.sentinel.project;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {}
